77111018f12237c8a1daca43eda8f596c755e0ba
zachradu <zachradu@10.182.149.39>
2020-10-05 22:53:42 -0700

    fixed code to work

--- cse360assignment01/src/cse360assignment01/AddingMachine.java
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -2,35 +2,51 @@ package cse360assignment01;
 
 public class AddingMachine
 {
-  private int total;
+  private static int total;
+  private static String output;
   
   public AddingMachine()
   {
     total = 0;  // not needed - included for clarity
+    output = "0 ";
   }
   
   public int getTotal()
   {
-    return 0;
+    return total;
   }
   
   public void add(int value)
   {
-	  
+	  total += value;
+	  output = output + "+ " + value + " ";
   }
 
   public void subtract(int value)
   {
-	  
+	  total -= value;
+	  output = output + "- " + value + " ";
   }
 
   public String toString()
   {
-    return "";
+    return output;
   }
 
   public void clear()
   {
-	  
+	  total = 0;
+	  output = "0 ";
+  }
+  
+  public static void main(String[] args)
+  {
+	  AddingMachine myCalculator = new AddingMachine();
+	  myCalculator.add(4); 
+	  myCalculator.clear();
+	  myCalculator.subtract (2); 
+	  myCalculator.add(5);
+	  System.out.println(myCalculator.getTotal());
+	  System.out.println(myCalculator.toString());
   }
 }
